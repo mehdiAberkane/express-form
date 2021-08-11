2252ebaa72d7399a929c429958d374d391bea2ad
ABERKANE Medhi (EXTERNE) <Medhi.ABERKANE.ext@ag2rlamondiale.fr>
2021-08-11 17:17:31 +0200

    some stuff

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fileUpload = require('express-fileupload')
 const path = require('path')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 3000
+const port = 80
 
 app.use(fileUpload({
   createParentPath: true
