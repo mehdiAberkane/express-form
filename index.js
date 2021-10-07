614117b7046a0baf92e516704aff2df74a1b8c96
ABERKANE Medhi (EXTERNE) <medhi.aberkane.ext@ag2rlamondiale.fr>
2021-10-07 16:21:18 +0200

    some stuff

--- index.js
+++ index.js
@@ -1,26 +1,42 @@
 const express = require('express')
-const fileUpload = require('express-fileupload')
 const path = require('path')
-const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 80;
+const aws = require('aws-sdk')
+const unirest = require('unirest')
 
-app.use(fileUpload({
-  createParentPath: true
-}))
+let env_var = new aws.S3({
+  token: process.env.TOKEN,
+  url_scanner: process.env.URL_SCANNER
+});
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.json())
+app.use(express.urlencoded())
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname+'/index.html'))
 })
 
 app.post('/', (req, res) => {
-  console.log(req.files)
-  res.sendFile(path.join(__dirname+'/index.html'))
+    console.log(req.body.url_val)
+
+    var req_post = unirest("POST", env_var.config.url_scanner);
+
+    req_post.headers({
+      "authorization": "bearer " + env_var.config.token,
+      "content-type": "application/json"
+    });
+    
+    req_post.type("json");
+    req_post.send({
+      "url": req.body.url_val
+    });
+
+    res.set('Content-Type', 'text/html');
+
+    req_post.end(function (res_post) {
+      res.send(Buffer.from('<body>status: '+ res_post.body.status + '<br>Matches:' + res_post.body.matches.toString()));
+    });
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(port, () => {})
